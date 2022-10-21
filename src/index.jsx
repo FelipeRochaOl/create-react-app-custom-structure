d6bc364f24e15008dd51ee36a2de979628b30407
Felipe Rocha <feliperochaoliveira@gmail.com>
2022-10-20 21:57:29 -0300

    Add folder to organize the components

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import { render } from "react-dom";
-import { App } from './App';
+import { App } from './components/App';
 
-import './styles/global'
+import './styles/global';
 
-render(<App />, document.getElementById('root'))
+render(<App />, document.getElementById('root'));
